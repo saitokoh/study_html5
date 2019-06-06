289e24cc6eebf800da594e41fc8e78c50e0ae15e
saitokoh <saitokoh@outlook.jp>
2019-06-06 15:51:44 +0900

    STEP1 線を描けるようにする

--- canvas/src/lib/paint.js
+++ canvas/src/lib/paint.js
@@ -1,2 +1,43 @@
 (function() {
+    // canvas取得
+    const canvas = document.querySelector('#paint');
+    const canvasPoint = canvas.getBoundingClientRect();
+
+    // コンテキスト取得
+    const ctx = canvas.getContext("2d");
+
+    // 線の初期設定
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = "000000";
+    ctx.lineWidth = 10;
+
+    let drawing = false; // true:描画中, false:Not描画中
+    canvas.addEventListener('mousedown', startDraw);
+    canvas.addEventListener('mouseup', endDraw);
+    canvas.addEventListener('mouseout', endDraw);
+    canvas.addEventListener('mousemove', draw);
+    
+    // 線描画関数
+    function draw(e) {
+        if (drawing) {
+            endX = e.clientX - canvasPoint.left + document.scrollingElement.scrollLeft;
+            endY = e.clientY - canvasPoint.top + document.scrollingElement.scrollTop;
+            ctx.beginPath();
+            ctx.moveTo(startX,startY);
+            ctx.lineTo(endX, endY);
+            ctx.stroke();
+            ctx.closePath();
+            startX = endX;
+            startY = endY;
+        }
+    }
+    function startDraw(e) {
+        drawing = true;
+        startX = e.clientX - canvasPoint.left + document.scrollingElement.scrollLeft;
+        startY = e.clientY - canvasPoint.top + document.scrollingElement.scrollTop;
+        draw(e);
+    }
+    function endDraw() {
+        drawing = false;
+    }
 })();
