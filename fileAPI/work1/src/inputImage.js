c2c087f2c26e7ac89268faac8440258ca54f484c
saito.kohei <saito.kohei@raccoon.ne.jp>
2019-07-03 13:10:09 +0900

    add file api answer

--- fileAPI/work1/src/inputImage.js
+++ fileAPI/work1/src/inputImage.js
@@ -2,23 +2,60 @@
     // 画像表示領域の取得
     const imageInsertDiv = document.querySelector('#image_area');
 
+    const errorMessage = {
+        noFile: "ファイルを１つ以上選択してください。",
+        overFileSize: "ファイルサイズは500kbyteまでです。",
+        wrongFileFormat: "ファイル形式はjpegかpngのみです。"
+    }
+
+    const createError = errorStr => {
+        const errorP = document.createElement('p');
+        errorP.textContent = errorStr;
+        return errorP;
+    }
+
     // input[type=file]のイベント設定
     document.querySelector('#image_input').addEventListener('change', e => {
-        // 1. FileListオブジェクトを取得
+        // イメージ貼り付け領域の初期化
+        while (imageInsertDiv.firstChild) {
+            imageInsertDiv.removeChild(imageInsertDiv.firstChild);
+        }
+
         const fileList = e.target.files;
-        // 2. FileListオブジェクトからFileオブジェクトを取得
-        const file = fileList[0];
+        imageInsertDiv.removeChild
 
-        const fileReader = new FileReader();
-        fileReader.onload = e => {
-            // 4. 読み込んだデータをあれこれする（画面に表示する）
-            const result = e.target.result;
-            const img = document.createElement('img');
-            img.src = result;
-            imageInsertDiv.appendChild(img);
-        };
+        // ファイル数チェック
+        if (fileList.length === 0) {
+            imageInsertDiv.appendChild(createError(errorMessage.noFile));
+            return;
+        }
 
-        // 3. FileReaderAPIでFileオブジェクトのファイルを実際に読み込む
-        fileReader.readAsDataURL(file);
+        // ファイルチェック
+        for(let i = 0 ; i < fileList.length ; i++) {
+            const file = fileList[i];
+            // ファイルサイズチェック
+            if (file.size > 500000 ) {
+                imageInsertDiv.appendChild(createError(errorMessage.overFileSize));
+                return;
+            }
+            // ファイル種類チェック
+            if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
+                imageInsertDiv.appendChild(createError(errorMessage.wrongFileFormat));
+                return;
+            }
+        }
+        
+        // ファイル読み込み->表示
+        for (let i = 0 ; i < fileList.length ; i++) {
+            const fileReader = new FileReader();
+            fileReader.onload = e => {
+                const result = e.target.result;
+                const img = document.createElement('img');
+                img.src = result;
+                imageInsertDiv.appendChild(img);
+            };
+            fileReader.readAsDataURL(fileList[i]);
+        }
+        
     });
 }())
--- fileAPI/work2/src/inputCsv.js
+++ fileAPI/work2/src/inputCsv.js
@@ -1,4 +1,103 @@
-(function(){
-    // ここに書いていく
-    
+(function () {
+    // Table領域の取得
+    const tableInsertDiv = document.querySelector('#table_area');
+
+    // 読み込まれているfile
+    let file;
+
+    const errorMessage = {
+        noFile: "ファイルを選択してください。",
+        overFileNum: "ファイルは１つだけ選択してください。",
+        wrongFileFormat: "CSVファイルを選択してください。"
+    }
+
+    const createError = errorStr => {
+        const errorP = document.createElement('p');
+        errorP.textContent = errorStr;
+        return errorP;
+    }
+
+    // 読み込みボタンクリックイベント
+    document.querySelector('#input_button').addEventListener('click', () => {
+        const fileList = document.querySelector('#csv_input').files;
+        tableInsertDiv.removeChild
+        // 読み込み数チェック
+        if (fileList.length === 0) {
+            tableInsertDiv.appendChild(createError(errorMessage.noFile));
+            return;
+        }
+        if (fileList.length > 1) {
+            tableInsertDiv.appendChild(createError(errorMessage.overFileNum));
+            return;
+        }
+
+        file = fileList[0];
+        // ファイルの拡張子チェック
+        if (!file.name.toLowerCase().endsWith('.csv')) {
+            tableInsertDiv.appendChild(createError(errorMessage.wrongFileFormat));
+            return;
+        }
+
+        // 初期化
+        while (tableInsertDiv.firstChild) {
+            tableInsertDiv.removeChild(tableInsertDiv.firstChild);
+        }
+
+        // ファイル読み込み->表示
+        const fileReader = new FileReader();
+        fileReader.onload = e => {
+            const table = csv2table(e.target.result)
+            tableInsertDiv.appendChild(table);
+        };
+        fileReader.readAsText(file);
+    });
+
+    // 保存ボタンのクリックイベント
+    document.querySelector('#save_button').addEventListener('click', () => {
+        const table = document.querySelector('table');
+        if (table === null) {
+            return;
+        }
+        const csv = table2csv(table);
+        const a = document.createElement('a');
+        a.href = 'data:text/plain,' + encodeURIComponent(csv);
+        a.download = file.name;
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    });
+
+    // csv -> table
+    function csv2table(csvStr) {
+        const table = document.createElement('table');
+        for (let row of csvStr.split("\n")) {
+            const tr = document.createElement('tr');
+            for (let value of row.split(",")) {
+                const td = document.createElement('td');
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.value = value;
+                td.appendChild(input)
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+        return table;
+    }
+
+    // table -> csv
+    function table2csv(table) {
+        let csv = "";
+        const trs = table.querySelectorAll('tr');
+        for (const tr of trs) {
+            const inputs = tr.querySelectorAll('input');
+            for (const input of inputs) {
+                csv += input.value + ',';
+            }
+            // 最後のカンマを取り除いて改行記号にする
+            csv = csv.slice(0, -1) + '\n';
+        }
+        return csv;
+    }
 }())
